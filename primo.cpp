8438df12fac98e11c0c161ba118095420ab8007a
Helias Torres Oliveira <helias.torres12@gmail.com>
2025-07-03 08:01:19 -0300

    adicionei primo.cpp

---
+++ primo.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int numero;
+
+    cin >> numero;
+
+    if (numero <= 1){
+
+        cout << "Nao eh primo" << endl;
+
+        return 0;
+    }
+}
