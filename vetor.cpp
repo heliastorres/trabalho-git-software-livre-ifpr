038c9fd9dce24a34a778ec5ddf6f90bef9c0f8d3
Helias Torres Oliveira <helias.torres12@gmail.com>
2025-07-03 08:29:07 -0300

    adicionei arquivo vetor.cpp

---
+++ vetor.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x, maior = 0;
+
+    cin >> x;
+
+    int vetor[x];
+
+    for (int i = 0; i < x; i++){
+
+        cin >> vetor[i];
+
+
+     return 0;
+}
